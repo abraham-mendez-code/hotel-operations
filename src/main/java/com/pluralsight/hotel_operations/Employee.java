bebe164fb40449bcaff498309bceb3e03a1ddb27
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-20 11:30:49 -0400

    Added the Employee class

---
+++ src/main/java/com/pluralsight/hotel_operations/Employee.java
@@ -0,0 +1,22 @@
+package com.pluralsight.hotel_operations;
+
+public class Employee {
+    private String employeeID;
+    private String name;
+    private String department;
+    private double payRate;
+    private double hoursWorked;
+
+    public double getTotalPay() {
+
+    }
+
+    public double getRegularHours() {
+
+    }
+
+    public double getOvertimeHours() {
+
+    }
+
+}
