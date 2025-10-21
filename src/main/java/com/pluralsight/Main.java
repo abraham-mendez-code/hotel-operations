eabc5281f8ccadbd36dc1c1a0c9a95fc7200c4cf
abraham-mendez-code <abraham.me001@gmail.com>
2025-10-21 13:38:25 -0400

    Refactored demo for the punching the time card, add demo for punching with the overloaded methods.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -112,7 +112,7 @@ public class Main {
         //print total pay
         System.out.println("Total pay: " + someEmployee.getTotalPay());
 
-        // test the punchTimeCard method
+        // test the punchIn and punchOut methods
 
         //get the current time
         LocalTime localTime = LocalTime.now();
@@ -120,18 +120,22 @@ public class Main {
         // get the current time as a double
         double time = (localTime.getHour() + (double) localTime.getMinute() / 60);
 
-        // punch the time card
-        someEmployee.punchTimeCard(time);
+        // punch in
+        someEmployee.punchIn(time);
+
+        // get another value for the time, e.g 8 hours from now
+        time = (localTime.getHour() + 8) + (double) localTime.getMinute() / 60;
+
+        // punch out
+        someEmployee.punchOut(time);
 
         // print out punch in and punch out values
         System.out.printf("Punched in: %.2f\n", someEmployee.getPunchIn());
         System.out.printf("Punched out: %.2f\n", someEmployee.getPunchOut());
 
-        // get another value for the time, e.g 8 hours from now
-        time = (localTime.getHour() + 8) + (double) localTime.getMinute() / 60;
-
-        // punch the time card again
-        someEmployee.punchTimeCard(time);
+        // use the overloaded methods to punch in and out
+        someEmployee.punchIn();
+        someEmployee.punchOut();
 
         // print out punch in and punch out values
         System.out.printf("Punched in: %.2f\n", someEmployee.getPunchIn());
