20e7aa1e15f3eebd8ff257e18eb4c487fb30e507
abraham-mendez-code <abraham.me001@gmail.com>
2025-10-21 09:40:46 -0400

    Added the Person class to describe a Person in the hotel

---
+++ src/main/java/com/pluralsight/Person.java
@@ -0,0 +1,40 @@
+package com.pluralsight;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    // constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // getters and setters
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // other methods
+    public String convertToString() {
+        return this.name + " is " + this.age + " year(s) old";
+    }
+
+    public boolean isAdult(){
+        return this.age > 18;
+    }
+
+}
