264264a68456cec27687102b026c15cc39a87c40
abraham-mendez-code <abraham.me001@gmail.com>
2025-10-21 14:53:36 -0400

    Added demos for Hotel class bookRoom() , getAvailableRooms() and getAvailableSuites() methods.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -6,126 +6,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //testing our room
-        Room someRoom = new Room(2, 420.0, false, false);
+        Hotel hotel = new Hotel("Hotel", 5, 5, 0, 0);
 
-        //if statement checking if the room is available
-        if(someRoom.isAvailable()){
-            System.out.println("The room is available");
-        }else{
-            System.out.println("The room is not available");
-        }
+        System.out.println(hotel.getAvailableRooms()); //show the avail rooms
+        System.out.println(hotel.getAvailableSuites()); //show the avail suites
 
-        //test the checkIn method
-        someRoom.checkIn();
+        System.out.println(hotel.bookRoom(1, true)); //book a suite
 
-        //tell user they are checking in
-        System.out.println("Checking in!");
+        System.out.println(hotel.bookRoom(1, false)); //book a basic room
 
-        //check if the room is available
-        if(someRoom.isAvailable()){
-            System.out.println("The room is available");
-        }else{
-            System.out.println("The room is not available");
-        }
+        System.out.println(hotel.getAvailableRooms()); //show the avail rooms
+        System.out.println(hotel.getAvailableSuites()); //show the avail suites
 
-        //test the checkOut method
-        someRoom.checkOut();
+        System.out.println(hotel.bookRoom(100, true)); //book too many suites
+        System.out.println(hotel.bookRoom(100, false)); //book too many basic rooms
 
-        System.out.println("Checking out!");
+        System.out.println(hotel.getAvailableRooms()); //show the avail rooms
+        System.out.println(hotel.getAvailableSuites()); //show the avail suites
 
-        if(someRoom.isAvailable()){
-            System.out.println("The room is available");
-        }else{
-            System.out.println("The room is not available");
-        }
-
-        //testing the reservation
-        Reservation someReservation = new Reservation("double", 3, false);
-
-        //check the price
-        System.out.println("Room price: " + someReservation.getPrice());
-
-        //check the reservation total
-        System.out.println("Reservation total: " + someReservation.getReservationTotal());
-
-        //change to a weekend
-        someReservation.setIsWeekend(true);
-
-        System.out.println("Its the weekend!");
-
-        //check the price
-        System.out.println("Room price: " + someReservation.getPrice());
-
-        //check the reservation total
-        System.out.println("Reservation total: " + someReservation.getReservationTotal());
-
-        //change number of nights
-        someReservation.setNumberOfNights(2);
-        System.out.println("Booking for " + someReservation.getNumberOfNights() + " nights");
-
-        //check the reservation total
-        System.out.println("Reservation total: " + someReservation.getReservationTotal());
-
-
-        //test the employee
-        Employee someEmployee = new Employee("JSN1", "Jason", "Front Desk", 10.00, 40);
-
-        // print out name
-        System.out.println(someEmployee.getName());
-
-        //print out regular hours
-        System.out.println("Worked " + someEmployee.getRegularHours() + " regular hours");
-
-        //print out OT hours
-        System.out.println("Worked " + someEmployee.getOvertimeHours() + " overtime hours");
-
-        //print total pay
-        System.out.println("Total pay: " + someEmployee.getTotalPay());
-
-        //add another hour so there is overtime
-        someEmployee.setHoursWorked(41);
-
-        // print out name
-        System.out.println(someEmployee.getName());
-
-        //print out regular hours
-        System.out.println("Worked " + someEmployee.getRegularHours() + " regular hours");
-
-        //print out OT hours
-        System.out.println("Worked " + someEmployee.getOvertimeHours() + " overtime hours");
-
-        //print total pay
-        System.out.println("Total pay: " + someEmployee.getTotalPay());
-
-        // test the punchIn and punchOut methods
-
-        //get the current time
-        LocalTime localTime = LocalTime.now();
-
-        // get the current time as a double
-        double time = (localTime.getHour() + (double) localTime.getMinute() / 60);
-
-        // punch in
-        someEmployee.punchIn(time);
-
-        // get another value for the time, e.g 8 hours from now
-        time = (localTime.getHour() + 8) + (double) localTime.getMinute() / 60;
-
-        // punch out
-        someEmployee.punchOut(time);
-
-        // print out punch in and punch out values
-        System.out.printf("Punched in: %.2f\n", someEmployee.getPunchIn());
-        System.out.printf("Punched out: %.2f\n", someEmployee.getPunchOut());
-
-        // use the overloaded methods to punch in and out
-        someEmployee.punchIn();
-        someEmployee.punchOut();
-
-        // print out punch in and punch out values
-        System.out.printf("Punched in: %.2f\n", someEmployee.getPunchIn());
-        System.out.printf("Punched out: %.2f\n", someEmployee.getPunchOut());
 
     }
 
